9ad077ae4ec196a90fb8fdda45eff1fcab4b5c3f
@Mustang95 <luhenlir@gmail.com>
2020-03-24 17:27:51 -0300

    icons and learn components

---
+++ layouts/default.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar fixed>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+      <nuxt></nuxt>
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app> </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire"
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-layout column justify-center align-center>
+    <div class="largeMargintop"></div>
+    <v-col>
+      <v-row>
+        <v-col cols="4">
+          <form action="">
+            <v-text-field
+              v-model="name"
+              :counter="10"
+              label="Nome da Tarefa"
+              solo
+            ></v-text-field>
+            <v-text-field
+              v-model="description"
+              :counter="10"
+              label="Descrição da Tarefa"
+              solo
+            ></v-text-field>
+            <v-btn color="success" @click.prevent="onSubmit" class="mr-4 red"
+              >Gravar Tarefas</v-btn
+            >
+          </form>
+        </v-col>
+        <v-col cols="8">
+          <h2>Lista de Tarefas</h2>
+          <p v-if="!tasks.length">Você não tem tarefas</p>
+          <v-card class="mx-auto" max-width="auto" tile>
+            <v-list-item v-for="(task, index) in tasks" :key="index">
+              <v-list-item-content>
+                <v-list-item-title>{{ task.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{
+                  task.description
+                }}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-icon>
+                  <v-icon @click="onRemove(index)">email</v-icon>
+                </v-list-item-icon>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-layout>
+</template>
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  data: () => ({
+    name: "",
+    description: "",
+    tasks: []
+  }),
+  methods: {
+    onSubmit() {
+      let productTask = {
+        name: this.name,
+        description: this.description
+      };
+      this.addTask(productTask);
+      //productTask.name = null;
+      //productTask.description = null;
+    },
+    addTask(productTask) {
+      this.tasks.push(productTask);
+    },
+    onRemove(index) {
+      debugger;
+      this.tasks.splice(index, 1);
+    },
+    onDelete() {
+      debugger;
+    }
+  }
+};
+</script>
+<style>
+.largeMargintop {
+  margin-top: 3rem;
+}
+</style>
